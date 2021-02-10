b0bc79bdeb5575b9560404fea45c70d9a9c4e486
himeshchopra <hm.ch7890@gmail.com>
2021-02-10 15:37:51 -0500

    updated arithmetic-demo.sh

--- bash/arithmetic-demo.sh
+++ bash/arithmetic-demo.sh
@@ -18,3 +18,22 @@ $firstnum plus $secondnum is $sum
 $firstnum divided by $secondnum is $dividend
   - More precisely, it is $fpdividend
 EOF
+
+#Remove the assignments of numbers to the first and second number variables. Use one or more read commands to get 3 numbers from the user
+echo ""
+Prompt="Please Enter Three Numbers: "
+read -p "$Prompt" one
+read -p "$Prompt" two
+read -p "$Prompt" three
+echo "You entered $one, $two, $three"
+
+# Task 2: Change the output to only show:
+#    the sum of the 3 numbers with a label
+#    the product of the 3 numbers with a label
+
+sum=$(($one + $two + $three))
+product=$(($one * $two * $three))
+echo ""
+echo "Sum of $one, $two, $three is : $sum"
+echo "Product of $one, $two, $three is : $product"
+echo ""
