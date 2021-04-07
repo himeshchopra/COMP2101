888dc62573badd6fa9b966ffe389536bd71ebc57
Himesh Chopra <himeshchopra1998@gmail.com>
2021-04-07 18:08:14 -0400

    powershell upload
    all files

---
+++ powershell/ipconfig_lab3.ps1
@@ -0,0 +1,4 @@
+﻿get-ciminstance -class win32_networkadapterconfiguration -filter ipenabled=true |
+where { $_.Description -ne $null -or $_.Index -ne $null -or $_.IPAddress -ne $null -or $_.IPSubnet -ne $null -or $_.DNSDomain -ne $null -or $_.DNSServerSearchOrder -ne $null } |
+select Description, Index, IPAddress, IPSubnet, DNSDomain, DNSServerSearchOrder |
+format-table
